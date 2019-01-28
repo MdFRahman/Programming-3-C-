6ee211be8fe908ff9ee01bf99c01cb5bd138db37
MdFRahman <47093773+MdFRahman@users.noreply.github.com>
2019-01-27 22:51:00 -0500

    Add files via upload

---
+++ DataContextDemo/DataContextDemo/PersonalDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContextDemo
+{
+    class PersonalDetails
+    {
+        string _firstName = "Bob";
+        string _lastName = "Smith";
+        int _age = 25;
+        static PersonalDetails _details;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public static PersonalDetails GetDetails()
+        {
+            if (_details == null)
+                _details = new PersonalDetails();
+
+            return _details;
+        }
+    }
+}
